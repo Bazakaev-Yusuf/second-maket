939d050145263e0fbd246f053a8d335f743f7180
Yusuf <yusupkiller71@mail.ru>
2023-02-17 19:50:00 +0300

    first commit

---
+++ js/aaa.js
@@ -0,0 +1,49 @@
+let a1 = document.querySelector(".shop__choice-room_bg");
+         a1.onclick = function () {
+            this.classList.toggle("click");
+         };
+    a2 = document.querySelector(".shop__choice-room_bg2");
+    a2.onclick = function () {
+       this.classList.toggle("click");
+    };
+    a3 = document.querySelector(".shop__choice-room_bg3");
+    a3.onclick = function () {
+       this.classList.toggle("click");
+    };
+    a4 = document.querySelector(".shop__choice-room_bg4");
+    a4.onclick = function () {
+       this.classList.toggle("click");
+    };
+    a5 = document.querySelector(".shop__choice-room_bg5");
+    a5.onclick = function () {
+       this.classList.toggle("click");
+    };
+    a6 = document.querySelector(".shop__choice-room_bg6");
+    a6.onclick = function () {
+       this.classList.toggle("click");
+    };
+
+    let like = document.querySelector('.choice__button-block_jsLike');
+        like.onclick = function(){
+        this.classList.toggle('click');
+        };
+        like2 = document.querySelector('.choice__button-block_jsLike2');
+        like2.onclick = function(){
+        this.classList.toggle('click');
+        };
+        like3 = document.querySelector('.choice__button-block_jsLike3');
+        like3.onclick = function(){
+        this.classList.toggle('click');
+    };
+    let share = document.querySelector('.choice__button-block_jsShare');
+        share.onclick = function(){
+        this.classList.toggle('click');
+        };
+        share2 = document.querySelector('.choice__button-block_jsShare2');
+        share2.onclick = function(){
+        this.classList.toggle('click');
+        };
+        share3 = document.querySelector('.choice__button-block_jsShare3');
+        share3.onclick = function(){
+        this.classList.toggle('click');
+        };
